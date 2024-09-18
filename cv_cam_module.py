4a50946b83fd5ae4147470ea6b2efd9613a06ff5
Gabriel Leal <marqsleal@gmail.com>
2024-09-18 08:28:25 -0300

    Opencv Modulo Camera Update

--- cv_cam_module.py
+++ cv_cam_module.py
@@ -1,23 +1,25 @@
 import cv2 as cv
 
-VIDEO_PATH = 0 # 0 for WebCam
+VIDEO_PATH = 0  # 0 para a webcam
 height = 420
 width = 360
 
-cap = cv.VideoCapture(VIDEO_PATH) 
+cap = cv.VideoCapture(VIDEO_PATH)
 
-fourcc = cv.VideoWriter_fourcc(* 'XVID')
-saida = cv.VideoWriter('video_test.mp4', fourcc, 20.0, (width,height))
+fourcc = cv.VideoWriter_fourcc(*'XVID')
+saida = cv.VideoWriter('video_test.avi', fourcc, 20.0, (width, height))
 
 while True:
-    ret,video = cap.read()
+    ret, video = cap.read()
 
-    hsv = cv.cvtColor(video, cv.COLOR_BAYER_BG2HSV)
+    if not ret:
+        print("Falha na captura de vídeo.")
+        break
 
-    saida.write(hsv)
-
+    video = cv.resize(video, (width, height))
+    hsv = cv.cvtColor(video, cv.COLOR_BGR2HSV)
+    saida.write(video)
     cv.imshow('Video', video)
-
     key = cv.waitKey(15) & 0xFF
 
     if key == ord('q'):
